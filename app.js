6d20eb6e58c9fbe972aceda1a17170615ad870da
FlagonDragon <195494654+FlagonDragon@users.noreply.github.com>
2026-07-11 06:13:06 -0400

    add new message route and messages object

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
 // app.js
-// const path = require("node:path");
+const path = require("node:path");
 const express = require("express");
 
 const app = express();
 
-// app.set("views", path.join(__dirname, "views"));
-// app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-// const assetsPath = path.join(__dirname, "public");
-// app.use(express.static(assetsPath));
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+app.get("/", (req, res) => {
+    res.render("index")
+});
 
 app.get("/", (req, res) => res.send("Homepage"));
+
+app.get("/new", (req, res) => res.send("new message"));
+
 
 const PORT = 3000;
 
@@ -20,6 +40,6 @@ app.listen(PORT, (error) => {
         throw error;
     }
 
-    console.log(`Mini Message Beard on port ${PORT}`);
+    console.log(`Mini Message Board on port ${PORT}`);
 
 })
