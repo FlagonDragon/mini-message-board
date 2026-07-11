3208e6e942a364566fd3630b5d01ec974852a640
FlagonDragon <195494654+FlagonDragon@users.noreply.github.com>
2026-07-11 05:03:37 -0400

    set up basic server

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
-const path = require("node:path");
-const express = require_("express");
+// app.js
+// const path = require("node:path");
+const express = require("express");
 
 const app = express();
 
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "ejs");
+
+// const assetsPath = path.join(__dirname, "public");
+// app.use(express.static(assetsPath));
+
+app.get("/", (req, res) => res.send("Homepage"));
+
+const PORT = 3000;
+
+app.listen(PORT, (error) => {
+
+    if (error) {
+        throw error;
+    }
+
+    console.log(`Mini Message Beard on port ${PORT}`);
+
+})
